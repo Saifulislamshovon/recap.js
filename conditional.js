eef33a10fd1371d4d116a7682852ef9d3ec28e3f
shovon <mdsaifulislamshovon@gmail.com>
2025-10-30 15:34:26 -0500

    first commit

---
+++ conditional.js
@@ -0,0 +1,10 @@
+function doMath(numb1, numb2, numb3){
+    const sum = numb1 + numb2 + numb3 ;
+    const diff = sum - numb3 ;
+    const mul = diff * numb3 ;
+    const divi = mul / 2 ;
+    const result = divi ;
+return result;
+}
+
+console.log(doMath(20,15,5));
