77228f406467559c501f20bbe8dd208ed2f87ad3
shovon <mdsaifulislamshovon@gmail.com>
2025-10-31 16:30:09 -0500

    1st commit

--- logic.js
+++ logic.js
@@ -32,4 +32,64 @@ console.log(("for male:" + ticketPrice('male') + "TK"));
 console.log(("for female:" + ticketPrice('female') + "TK"));
 console.log(("for others:" + ticketPrice('other') + "TK"));
 console.log(("for unknown:" + ticketPrice('X') + "TK"));
+console.log("-----------------------------------------");
 
+
+function halfOrTriple(num, half){ 
+    if(half === true){ 
+        return num / 2; 
+    } 
+    else { 
+        return num * 3;
+     } 
+    } 
+    
+    console.log("By Doing Half:" + halfOrTriple(10,true)); 
+    // 5 
+    console.log("By Doing Tripple:" + halfOrTriple(10,false)); 
+    // 30 
+console.log("-----------------------------------------");
+
+
+function halfOrTriple(num, half){ if(half === true){ 
+    return num / 2; 
+} 
+
+else { 
+    return num * 3; 
+    } 
+} 
+
+console.log(halfOrTriple(10,true)); 
+// 5 
+console.log(halfOrTriple(10,false)); 
+// 30 
+
+function doOrDie(number, Good){
+    if (number>40 === true ){
+        return "Passed"
+    }
+    else  {
+            return "Please die"
+    }
+}
+
+console.log(doOrDie(30, true));
+
+console.log("-----------------------------------------");
+
+function checkDiscount(age, isStudent, hasCoupon) {
+    if (age <= 35 && (isStudent === true && hasCoupon === true)) {
+        return "Special Price"; 
+    } else if (isStudent) {
+        return "10% Off";      
+    } else if (hasCoupon) {
+        return "5% Off";        
+    } 
+    // 4. Upore kono discount na pele, ei block-ti cholbe
+    else {                      
+        return "Sorry";
+    }
+}
+
+console.log("2. Discount:" + checkDiscount(25, true, false));
