3ae82c004d36047a0fe9f5f5a894be23673a5394
shovon <mdsaifulislamshovon@gmail.com>
2025-11-13 06:33:31 -0600

    1st commit

---
+++ for-loop.js
@@ -0,0 +1,9 @@
+const cars = ["A","B" , "C","D" ,"E ", "F","G" ,"H" ,"I" ];
+
+for (let i = 0; i < cars.length; i++) {
+    
+    const currentCar = cars[i]; 
+    
+    console.log(currentCar);
+    //console.log(`Index ${i} er man holo: ${currentCar}`);
+}
---
+++ forIn.js
@@ -0,0 +1,26 @@
+/*
+const salaries = {
+        john : 5500,
+        peteer : 2400,
+        james : 3400,
+}
+
+for(let x in salaries){
+       let salary =  `£${salaries[x]}`;
+       console.log(`${x} Ears in monthly £${salaries[x]} `);
+}
+
+*/
+
+const a = "Shovon"
+
+for (let x in a){
+        console.log(a[x]);
+}
+console.log("----------------------------------");
+console.log("----------------------------------");
+
+for (let z of a){
+        console.log(z);
+}
+
